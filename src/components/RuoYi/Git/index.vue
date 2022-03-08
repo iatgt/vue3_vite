da472281c333e5cdf54b533b7d1aae26e0619915
BagrationC <bagration_c@163.com>
2022-03-08 22:54:08 +0800

    修改了一些个人信息

--- src/components/RuoYi/Git/index.vue
+++ src/components/RuoYi/Git/index.vue
@@ -5,7 +5,8 @@
 </template>
 
 <script setup>
-const url = ref('https://gitee.com/y_project/RuoYi-Vue');
+// const url = ref('https://gitee.com/y_project/RuoYi-Vue');
+const url = ref('https://github.com/');
 
 function goto() {
   window.open(url.value)
